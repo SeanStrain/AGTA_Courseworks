b4bba98cb70d1ddd9b8025e13a75ad23df6a985b
S-A-Strain <seanstrainwork@gmail.com>
2022-03-02 12:14:20 +0000

    adding agta.py to git

---
+++ agta.py
@@ -0,0 +1,58 @@
+def agta_three():
+
+    player_one = ['h']
+    player_two = ['h']
+
+    p1h = 1 # times player 1 played h
+    p1t = 0 # times player 1 played t
+
+    p2h = 1 # times player 2 played h
+    p2t = 0 # times player 2 played t
+
+    i = 1
+    while i <= 100:
+        pr1h = p1h / i # probability of player 1 playing h
+        pr1t = p1t / i # probability of player 1 playing t
+
+        pr2h = p2h / i # probability of player 2 playing h
+        pr2t = p2t / i # probability of player 2 playing t
+
+        if pr2h > pr2t:
+            p = 'h'
+        elif pr2h < pr2t:
+            p = 't'
+        else:
+            p = 'h' if(player_two[-1] == 'h') else 't'
+
+        if p == 'h':
+            p1h += 1
+        else:
+            p1t += 1
+
+        player_one.append(p)
+
+        if pr1h < pr1t:
+            m = 'h'
+        elif pr1h > pr1t:
+            m = 't'
+        else:
+            m = 't' if(player_one[-1] == 'h') else 'h'
+
+        if m == 'h':
+            p2h += 1
+        else:
+            p2t += 1
+
+        player_two.append(m)
+        i += 1
+
+        print(pr1h*pr2h, p2h/p1t)
+
+    print(player_one)
+
+
+def main():
+    agta_three()
+
+if __name__ == '__main__':
+    main()
